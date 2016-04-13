ac54a7226c4edec357a1eab9e240259c11d97221
Joel-Haeberli <haebu@rubigen.ch>
2016-04-13 13:10:51 +0200

    Gui in Klasse "Register" integriert und somit wird Klasse "RegisterGUI" nicht mehr benötigt. Ab nächstem commit wird diese Klasse nicht mehr Committet

--- src/benutzerverwaltung/Register.java
+++ src/benutzerverwaltung/Register.java
@@ -1,9 +1,20 @@
 package benutzerverwaltung;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Register {
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Register implements ActionListener{
 	
 	private boolean istRichtig; //Variable welche in allen Validierungsfunktionen gebraucht wird.
 	private boolean istVorhanden; //Variable von Funktion "askDatabase()"
@@ -18,17 +29,23 @@ public class Register {
 	
 	private String Userdata; //Speichert alle Userdaten um Datenbank zu überprüfen
 	
-	private RegisterGUI getData = new RegisterGUI();
+	//Alle swas es fürs GUI braucht
+	JFrame RegisterFrame = new JFrame();
+	
+	JPanel pnPanel0;
+
+	JPanel All;
+	JButton Register;
+	JCheckBox Lehrer;
+	JTextField TUsername;
+	JTextField TEmail;
+	JTextField TPassword;
+	
+	JLabel Fehler;
 	
 	public Register() {
 		System.out.println("Constructor Register start");
-		getUsername();
-		getEmail();
-		getPassword();
-		System.out.println(Username);
-		System.out.println(Email);
-		System.out.println(Password);
-		generateUserdata();
+		generateGUI();
 		System.out.println("Constructor Register end");
 	}
 	
@@ -56,6 +73,13 @@ public class Register {
 	
 	public boolean validateLernender() {
 		istRichtig = false;
+		getUsername();
+		getEmail();
+		getPassword();
+		System.out.println(Username);
+		System.out.println(Email);
+		System.out.println(Password);
+		generateUserdata();
 		validateEmail();
 		validateUsername();
 		validatePassword();
@@ -70,6 +94,13 @@ public class Register {
 	
 	public boolean validateLehrer() {
 		istRichtig = false;
+		getUsername();
+		getEmail();
+		getPassword();
+		System.out.println(Username);
+		System.out.println(Email);
+		System.out.println(Password);
+		generateUserdata();
 		validateEmail();
 		validateUsername();
 		validatePassword();
@@ -100,7 +131,7 @@ public class Register {
 	private boolean validateUsername() {
 		//TODO
 		//Validierung Username
-		final String R_USERNAME = "^[_A-Za-z0-9-\\+]$";
+		final String R_USERNAME = "^[A-Za-z0-9]{4,30}$";
 		Pattern pUsername = Pattern.compile(R_USERNAME);
 		Matcher mUsername = pUsername.matcher(Username);
 		if (mUsername.matches()) {
@@ -115,7 +146,7 @@ public class Register {
 	private boolean validatePassword() {
 		//TODO
 		//Validierung Password
-		final String R_PASSWORD = "^[\\w]{8,50}$";
+		final String R_PASSWORD = "^[A-Za-z0-9!?+*,ç%&=]{8,50}$";
 		Pattern pPassword = Pattern.compile(R_PASSWORD);
 		Matcher mPassword = pPassword.matcher(Password);
 		if (mPassword.matches()) {
@@ -130,19 +161,19 @@ public class Register {
 	private void getUsername() {
 		//TODO
 		//Eingabe "Username" aus RegisterGUI holen
-		Username = getData.getUsernameField();
+		Username = TUsername.getText();
 	}
 	
 	private void getPassword() {
 		//TODO
 		//Eingabe "Password" aus RegisterGUI holen
-		Password = getData.getPasswordField(); 
+		Password = TPassword.getText();
 	}
 	
 	private void getEmail() {
 		//TODO
 		//Eingabe "Email" aus RegisterGUI holen
-		Email = getData.getEmailField();
+		Email = TEmail.getText();
 	}
 	
 	public void generateUserdata() {
@@ -163,5 +194,111 @@ public class Register {
 	private void setData(String registerData) {
 		//TODO 
 		//Abspeicherung in Datenbank programmieren -> mit Roger absprechen
+	}
+	
+	private void generateGUI() {
+		RegisterFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		RegisterFrame.setTitle("Registrieren");
+
+		pnPanel0 = new JPanel();
+		GridBagLayout gbPanel0 = new GridBagLayout();
+		GridBagConstraints gbcPanel0 = new GridBagConstraints();
+		pnPanel0.setLayout(gbPanel0);
+
+		All = new JPanel();
+		GridBagLayout gbAll = new GridBagLayout();
+		GridBagConstraints gbcAll = new GridBagConstraints();
+		All.setLayout(gbAll);
+
+		Register = new JButton("Registrieren");
+		gbcAll.gridx = 4;
+		gbcAll.gridy = 12;
+		gbcAll.gridwidth = 8;
+		gbcAll.gridheight = 2;
+		gbcAll.fill = GridBagConstraints.BOTH;
+		gbcAll.weightx = 1;
+		gbcAll.weighty = 0;
+		gbcAll.anchor = GridBagConstraints.NORTH;
+		gbAll.setConstraints(Register, gbcAll);
+		All.add(Register);
+
+		Lehrer = new JCheckBox("Sind Sie ein Lehrer?");
+		Lehrer.setSelected(true);
+		gbcAll.gridx = 7;
+		gbcAll.gridy = 10;
+		gbcAll.gridwidth = 1;
+		gbcAll.gridheight = 1;
+		gbcAll.fill = GridBagConstraints.BOTH;
+		gbcAll.weightx = 1;
+		gbcAll.weighty = 0;
+		gbcAll.anchor = GridBagConstraints.NORTH;
+		gbAll.setConstraints(Lehrer, gbcAll);
+		All.add(Lehrer);
+
+		TUsername = new JTextField();
+		gbcAll.gridx = 7;
+		gbcAll.gridy = 3;
+		gbcAll.gridwidth = 1;
+		gbcAll.gridheight = 1;
+		gbcAll.fill = GridBagConstraints.BOTH;
+		gbcAll.weightx = 1;
+		gbcAll.weighty = 0;
+		gbcAll.anchor = GridBagConstraints.NORTH;
+		gbAll.setConstraints(TUsername, gbcAll);
+		All.add(TUsername);
+
+		TEmail = new JTextField();
+		gbcAll.gridx = 7;
+		gbcAll.gridy = 5;
+		gbcAll.gridwidth = 1;
+		gbcAll.gridheight = 1;
+		gbcAll.fill = GridBagConstraints.BOTH;
+		gbcAll.weightx = 1;
+		gbcAll.weighty = 0;
+		gbcAll.anchor = GridBagConstraints.NORTH;
+		gbAll.setConstraints(TEmail, gbcAll);
+		All.add(TEmail);
+
+		TPassword = new JTextField();
+		gbcAll.gridx = 7;
+		gbcAll.gridy = 7;
+		gbcAll.gridwidth = 1;
+		gbcAll.gridheight = 1;
+		gbcAll.fill = GridBagConstraints.BOTH;
+		gbcAll.weightx = 1;
+		gbcAll.weighty = 0;
+		gbcAll.anchor = GridBagConstraints.NORTH;
+		gbAll.setConstraints(TPassword, gbcAll);
+		All.add(TPassword);
+		gbcPanel0.gridx = 2;
+		gbcPanel0.gridy = 1;
+		gbcPanel0.gridwidth = 16;
+		gbcPanel0.gridheight = 19;
+		gbcPanel0.fill = GridBagConstraints.BOTH;
+		gbcPanel0.weightx = 1;
+		gbcPanel0.weighty = 0;
+		gbcPanel0.anchor = GridBagConstraints.NORTH;
+		gbPanel0.setConstraints(All, gbcPanel0);
+		pnPanel0.add(All);
+
+		Register.addActionListener(this);
+
+		RegisterFrame.setContentPane(pnPanel0);
+		RegisterFrame.pack();
+		RegisterFrame.setVisible(true);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// Listener für den Registrierungsbutton
+		if (arg0.getSource() == Register) {
+			if (Lehrer.isSelected()) {
+				addLehrer();
+				System.out.println("Registrieren: Lernender");
+			} else {
+				addLernender();
+				System.out.println("Registrieren: Lehrer");
+			}
+		}
 	}
 }
